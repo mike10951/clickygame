99b6ce991f23c18ada8f70c7eaa8be5e786b0fca
Miguel Cabrera <miguel.cabrera.1095@gmail.com>
2020-02-05 12:02:46 -0600

    Minor fixes

---
+++ build/precache-manifest.f3c8862dfc478aed2ed7c051b175ff51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c9a67152f686dcf7496df7f845b26c8",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "bd0bd4b5a92a7d2105ea",
+    "url": "/clickygame/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f5f4730a36645cb26a4e",
+    "url": "/clickygame/static/js/2.e3618148.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clickygame/static/js/2.e3618148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd0bd4b5a92a7d2105ea",
+    "url": "/clickygame/static/js/main.344f8df0.chunk.js"
+  },
+  {
+    "revision": "4d95e025c74c73140b85",
+    "url": "/clickygame/static/js/runtime-main.2999711a.js"
+  }
+]);
