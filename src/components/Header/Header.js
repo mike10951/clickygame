af6207ffeb7305e0aacb366524633566d4382120
Miguel Cabrera <miguel.cabrera.1095@gmail.com>
2020-02-05 11:59:54 -0600

    Minor fixes

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="jumbotron">
+      <h1>You have to touch every image, but don't repeat yourself!</h1>
+    </div>
+  );
+}
+
+export default Header;
